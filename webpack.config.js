11173f7e6c6bddd8bdd6a9e2dc3bdefde1024000
Iain MacDonald <iain.ilearner@gmail.com>
2019-10-10 12:37:45 +0800

    Prepare for production deploy

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,37 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const TerserJSPlugin = require('terser-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const devMode = process.env.NODE_ENV !== 'production';
+
 module.exports = {
+    optimization: {
+        minimizer: [
+            new TerserJSPlugin({}),
+            new OptimizeCSSAssetsPlugin({}),
+        ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: "HexViewer",
+            template: "index.html",
+            minify: { 
+                // https://github.com/kangax/html-minifier#options-quick-reference 
+                collapseWhitespace: true, 
+                removeComments: true, 
+                removeRedundantAttributes: true, 
+                removeScriptTypeAttributes: true, 
+                removeStyleLinkTypeAttributes: true, 
+                useShortDoctype: true,
+            },
+        }),
+        new MiniCssExtractPlugin({
+            // Options similar to the same options in webpackOptions.output
+            // both options are optional
+            filename: devMode ? '[name].css' : '[name].[hash].css',
+            chunkFilename: devMode ? '[id].css' : '[id].[hash].css',
+        }),
+    ],
     resolve: {
         extensions: [".jsx",".js"],
     },
@@ -16,7 +49,16 @@ module.exports = {
             },
             {
                 test: /\.css$/i,
-                use: ['style-loader', 'css-loader'],
+                use: [
+                    {
+                        loader: MiniCssExtractPlugin.loader,
+                        options: {
+                            hmr: process.env.NODE_ENV === 'development',
+                        },
+                    },
+                    'css-loader',
+                    'postcss-loader',
+                ],
             },
         ],
     },
