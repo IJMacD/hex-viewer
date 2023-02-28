6f0a259083369bcd1bbbbf7a1d0c2fca03183c36
Iain MacDonald <ijmacd@gmail.com>
2023-02-28 12:13:06 +0800

    AnnotationEditor can actually edit

--- src/AnnotationEditor.jsx
+++ src/AnnotationEditor.jsx
@@ -4,6 +4,7 @@ import "./AnnotationEditor.css";
 const types = [ "bytes", "ASCII", "UTF-8", "Uint8", "Uint16", "Uint32", "Uint64" ];
 
 export default function AnnotationEditor ({ template = [], setTemplate }) {
+    const [ editIndex, setEditIndex ] = useState(-1);
 
     function handleRemove (index) {
         setTemplate([ ...template.slice(0, index), ...template.slice(index + 1) ]);
@@ -13,34 +14,50 @@ export default function AnnotationEditor ({ template = [], setTemplate }) {
         setTemplate([ ...(template||[]), newAnnotation ]);
     }
 
+    function handleSave (annotation, index) {
+        setTemplate([ ...template.slice(0, index), annotation, ...template.slice(index + 1) ]);
+        setEditIndex(-1);
+    }
+
     function handleExport () {
         console.log(JSON.stringify(template));
         alert("Written to console");
     }
 
+    function handleSubEdit (index, children) {
+        const annotation = { ...template[index], children };
+        setTemplate([ ...template.slice(0, index), annotation, ...template.slice(index + 1) ]);
+    }
+
     return (
         <div className="AnnotationEditor">
             {
-                (template||[]).map((a,i) => <Annotation key={i} annotation={a} onRemove={() => handleRemove(i)} />)
+                (template||[]).map((a,i) => {
+                    if (i === editIndex) {
+                        return <EditAnnotation key={i} annotation={a} addAnnotation={a => handleSave(a, i)} onCancel={() => setEditIndex(-1)} />;
+                    }
+                    return <Annotation key={i} annotation={a} onRemove={() => handleRemove(i)} onEdit={() => setEditIndex(i)} editChildren={children => handleSubEdit(i, children)} />;
+                })
             }
-            <NewAnnotation addAnnotation={handleNewAnnotation} />
+            <EditAnnotation addAnnotation={handleNewAnnotation} />
             <button onClick={handleExport}>Export</button>
         </div>
     );
 }
 
-function Annotation ({ annotation, onRemove }) {
+function Annotation ({ annotation, onRemove, onEdit, editChildren }) {
     if (annotation.children) {
         return (
-            <div style={{ border: "1px solid #ccc", padding: 4, position: "relative" }}>
-                {annotation.label}
+            <div className="AnnotationEditor-Annotation">
+                <p>{annotation.label}</p>
+                <AnnotationEditor template={annotation.children} setTemplate={editChildren} />
                 <button onClick={onRemove} style={{ position: "absolute", top: 8, right: 8 }}>Remove</button>
             </div>
         );
     }
 
     return (
-        <div style={{ border: "1px solid #ccc", padding: 4, position: "relative" }}>
+        <div className="AnnotationEditor-Annotation">
             <dl>
                 {
                     Object.entries(annotation).filter(ddFilter).map(([key, value]) => (
@@ -51,6 +68,7 @@ function Annotation ({ annotation, onRemove }) {
                     ))
                 }
             </dl>
+            <button onClick={onEdit} style={{ position: "absolute", top: 32, right: 8 }}>Edit</button>
             <button onClick={onRemove} style={{ position: "absolute", top: 8, right: 8 }}>Remove</button>
         </div>
     );
@@ -62,54 +80,67 @@ function ddFilter ([key, value]) {
         || (typeof value === "boolean" && value);
 }
 
-function NewAnnotation ({ addAnnotation }) {
-    const [ label, setLabel ] = useState("");
-    const [ type, setType ] = useState("");
-    const [ start, setStart ] = useState(0);
-    const [ length, setLength ] = useState(0);
-    const [ enableStart, setEnableStart ] = useState(false);
-    const [ enableLength, setEnableLength ] = useState(false);
-    const [ littleEndian, setLittleEndian ] = useState(false);
+/**
+ *
+ * @param {object} props
+ * @param {(object) => void} props.addAnnotation
+ * @param {import("./annotate").Annotation} [props.annotation]
+ * @param {() => void} [props.onCancel]
+ * @returns
+ */
+function EditAnnotation ({ addAnnotation, annotation, onCancel }) {
+    const [ label, setLabel ] = useState(annotation?.label || "");
+    const [ id, setID ] = useState(annotation?.id || "");
+    const [ type, setType ] = useState(annotation?.type || "");
+    const [ start, setStart ] = useState(annotation?.start || 0);
+    const [ length, setLength ] = useState(annotation?.length || 0);
+    const [ enableStart, setEnableStart ] = useState(typeof annotation?.start === "number");
+    const [ enableLength, setEnableLength ] = useState(typeof annotation?.length === "number");
+    const [ littleEndian, setLittleEndian ] = useState(typeof annotation?.littleEndian === "boolean");
 
     function handleAdd () {
         addAnnotation({
             label,
+            id: id || void 0,
             type,
             start: enableStart ? start : void 0,
             length: enableLength ? length : void 0,
             littleEndian: littleEndian || void 0,
         });
         setLabel("");
-        // setStart(0);
-        // setLength(0);
-        // setEnableStart(false);
-        // setEnableLength(false);
+        setID("");
     }
 
     return (
-        <div>
-            <label>Label
+        <div className="AnnotationEditor-EditAnnotation">
+            <label><span>Label</span>
                 <input value={ label } onChange={e => setLabel(e.target.value)} />
             </label>
-            <label>Type
+            <label><span>Type</span>
                 <select value={ type } onChange={e => setType(e.target.value)}>
                     {
                         types.map(t => <option key={t} value={t}>{t}</option>)
                     }
                 </select>
             </label>
-            <label>Start
+            <label><span>ID</span>
+                <input value={ id } onChange={e => setID(e.target.value)} />
+            </label>
+            <label><span>Start</span>
                 <input type="checkbox" checked={enableStart} onChange={e => setEnableStart(e.target.checked)} />
                 <input value={ start } onChange={e => setStart(+e.target.value)} type="number" disabled={!enableStart} />
             </label>
-            <label>Length
+            <label><span>Length</span>
                 <input type="checkbox" checked={enableLength} onChange={e => setEnableLength(e.target.checked)} />
                 <input value={ length } onChange={e => setLength(+e.target.value)} type="number" disabled={!enableLength} />
             </label>
-            <label>Little Endian
+            <label><span>Little Endian</span>
                 <input type="checkbox" checked={littleEndian} onChange={e => setLittleEndian(e.target.checked)} />
             </label>
-            <button onClick={handleAdd}>Add</button>
+            <div style={{textAlign:"right"}}>
+                { onCancel && <button onClick={onCancel}>Cancel</button> }
+                <button onClick={handleAdd}>{annotation?"Save":"Add"}</button>
+            </div>
         </div>
     );
 }
